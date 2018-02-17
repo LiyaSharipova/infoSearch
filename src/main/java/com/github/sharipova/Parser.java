b41a8cff67ee84f177f42a4e99341d0419ab0145
liyasharipova <liyasharipova2107@gmail.com>
2018-02-18 01:37:36 +0300

    abstract added

--- src/main/java/com/github/sharipova/Parser.java
+++ src/main/java/com/github/sharipova/Parser.java
@@ -7,7 +7,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
@@ -16,8 +19,7 @@ public class Parser {
     public static void main(String[] args) throws IOException {
         JSONObject obj = new JSONObject();
         JSONArray articles = new JSONArray();
-        JSONArray article = new JSONArray();
-        JSONArray keyWords = new JSONArray();
+        JSONObject article = new JSONObject();
 
         Document doc = Jsoup.connect("http://www.mathnet.ru/php/archive.phtml?jrnid=uzku&wshow=issue&bshow=contents&series=0&year=2016&volume=158%22%20\\%20%22&issue=1&option_lang=rus&bookID=1621").get();
         Elements links = doc.select("td[colspan] > a.SLink");
@@ -25,11 +27,24 @@ public class Parser {
         for (Element link : links) {
             String url = SITE + link.attr("href");
             doc = (Document) Jsoup.connect(url).get();
-            article.add("URL : " + url);
-            article.add("Title : " + doc.select("span.red:has(font)").text());
+            article.put("URL", url);
+            article.put("Title", doc.select("span.red:has(font)").text());
+            Pattern pattern = Pattern.compile("<b>Аннотация:<\\/b>(.|\\s)*?<br>");
+            Matcher matcher = pattern.matcher(doc.body().toString());
+            String group = matcher.group(1);
+
+            boolean matches = doc.body().toString().matches("<b>Аннотация:<\\/b>(.|\\s)*?<br>");
 
 //            /html/body/table[1]/tbody/tr/td/table[3]/tbody/tr/td[2]/text()[2]
-            article.add("Abstract : " + doc.select("b:contains(Аннотации) ~ b:contains(Ключевые слова)"));
+            Elements elements = doc.select("b:contains(Аннотация:)");
+            String s = elements.first().nextSibling().toString();
+            article.put("Abstract" , doc.select("body:matches(<b>Аннотация:<\\/b>(.|\\s)*?<br>)"));
+
+            article.put("Keywords", doc.select("b + i").text());
+            articles.add(article);
         }
+//        try (FileWriter file = new FileWriter("result.txt")) {
+//            file.write(obj.toJSONString());
+//        }
     }
 }
