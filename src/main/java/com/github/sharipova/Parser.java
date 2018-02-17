3c653f3140ae0208400362a4b97122329079bd94
liyasharipova <liyasharipova2107@gmail.com>
2018-02-18 02:23:00 +0300

    json to file added

--- src/main/java/com/github/sharipova/Parser.java
+++ src/main/java/com/github/sharipova/Parser.java
@@ -15,36 +15,36 @@ import java.util.regex.Pattern;
 public class Parser {
 
     public final static String SITE = "http://www.mathnet.ru";
+    public final static String HOME = "http://www.mathnet.ru/php/archive.phtml?jrnid=uzku&wshow=issue&bshow=contents&series=0&year=2016&volume=158%22%20\\%20%22&issue=1&option_lang=rus&bookID=1621";
 
     public static void main(String[] args) throws IOException {
         JSONObject obj = new JSONObject();
         JSONArray articles = new JSONArray();
         JSONObject article = new JSONObject();
 
-        Document doc = Jsoup.connect("http://www.mathnet.ru/php/archive.phtml?jrnid=uzku&wshow=issue&bshow=contents&series=0&year=2016&volume=158%22%20\\%20%22&issue=1&option_lang=rus&bookID=1621").get();
+        Document doc = Jsoup.connect(HOME).get();
         Elements links = doc.select("td[colspan] > a.SLink");
 
         for (Element link : links) {
             String url = SITE + link.attr("href");
             doc = (Document) Jsoup.connect(url).get();
-            article.put("URL", url);
-            article.put("Title", doc.select("span.red:has(font)").text());
             Pattern pattern = Pattern.compile("<b>Аннотация:<\\/b>(.|\\s)*?<br>");
             Matcher matcher = pattern.matcher(doc.body().toString());
-            String group = matcher.group(1);
+            String annotation = "";
+            if (matcher.find()) {
+                annotation = matcher.group(0).replace("<b>Аннотация:</b>", "").replace("<br>", "");
+            }
 
-            boolean matches = doc.body().toString().matches("<b>Аннотация:<\\/b>(.|\\s)*?<br>");
-
-//            /html/body/table[1]/tbody/tr/td/table[3]/tbody/tr/td[2]/text()[2]
-            Elements elements = doc.select("b:contains(Аннотация:)");
-            String s = elements.first().nextSibling().toString();
-            article.put("Abstract" , doc.select("body:matches(<b>Аннотация:<\\/b>(.|\\s)*?<br>)"));
 
             article.put("Keywords", doc.select("b + i").text());
+            article.put("Abstract", annotation);
+            article.put("URL", url);
+            article.put("Title", doc.select("span.red:has(font)").text());
             articles.add(article);
         }
-//        try (FileWriter file = new FileWriter("result.txt")) {
-//            file.write(obj.toJSONString());
-//        }
+        obj.put("Articles", articles);
+        try (FileWriter file = new FileWriter("result.json")) {
+            file.write(obj.toJSONString());
+        }
     }
 }
